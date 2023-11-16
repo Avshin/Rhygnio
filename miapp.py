25ac47a8abea4014ea64cc9eb15c7b91348f7aac
avshin <aveleloco@gmail.com>
2023-11-16 15:52:03 -0300

    as

--- miapp.py
+++ miapp.py
@@ -1,2 +1,19 @@
-from flask import Flask
-from psycopg2 import connection
+from flask import Flask, send_file
+from psycopg2 import connect
+
+app = Flask(__name__)
+
+host = 'localhost'
+port = 5432
+dbname = 'Rhy'
+user = 'postgres'
+password = 'a'
+
+def get_connection():
+    conn = connect (host=host, port=port, dbname=dbname, user=user, password=password)
+    return conn
+@app.route('/')
+def index():
+    return send_file("/")
+if __name__ == '__main__':
+    app.run(debug=True)
